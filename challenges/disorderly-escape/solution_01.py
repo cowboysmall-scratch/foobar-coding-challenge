9e2baa43a3867977bbde547b9ffaf0c1405da5a3
Jerry Kiely <jerry@cowboysmall.com>
2021-12-29 14:53:12 +0000

    some cleanup

--- challenges/disorderly-escape/solution_01.py
+++ challenges/disorderly-escape/solution_01.py
@@ -14,7 +14,6 @@ def solution(w, h, s):
     def lcm(p, q):
         return p * q // gcd(p, q)
 
-
     def cycle_combinations_rec(i, n, l, c, t = []):
         if n == 0:
             c.append(list(Counter(t[:l]).items()))
@@ -24,34 +23,33 @@ def solution(w, h, s):
             cycle_combinations_rec(j, n - j, l + 1, c, t)
             t.pop()
 
-
     def cycle_combinations(n):
         c = []
         cycle_combinations_rec(1, n, 0, c)
         return c
 
+    def calculate_coefficient(c):
+        cf = 1
+        for v, f in c:
+            cf *= v**f
+            cf *= factorial(f)
+        return cf
 
-    def get_coefficient(cycle):
-        coefficient = 1
-        for v, f in cycle:
-            coefficient *= factorial(f)
-            coefficient *= v**f
-        return coefficient
 
+    row_cycles = cycle_combinations(h)
+    col_cycles = cycle_combinations(w)
+    row_coeffs = [calculate_coefficient(cycle) for cycle in row_cycles]
+    col_coeffs = [calculate_coefficient(cycle) for cycle in col_cycles]
 
-    rows = [(cycle, get_coefficient(cycle)) for cycle in cycle_combinations(h)]
-    cols = [(cycle, get_coefficient(cycle)) for cycle in cycle_combinations(w)]
+    total = 0
 
-    count = 0
-
-    for row_cycle, row_coefficient in rows:
-        for col_cycle, col_coefficient in cols:
-            total = 1
+    for row_cycle, row_coeff in zip(row_cycles, row_coeffs):
+        for col_cycle, col_coeff in zip(col_cycles, col_coeffs):
+            term = Fraction(1, col_coeff * row_coeff)
             for v1, f1 in row_cycle:
                 for v2, f2 in col_cycle:
                     exp    = (v1 * f1 * v2 * f2) // lcm(v1, v2)
-                    total *= s**exp
+                    term *= s**exp
+            total += term
 
-            count += Fraction(total, col_coefficient * row_coefficient)
-
-    return str(count)
+    return str(total)
