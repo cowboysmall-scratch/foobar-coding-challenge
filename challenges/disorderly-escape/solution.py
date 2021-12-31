527715dc0e81b49134412ba46f9f1c4adf432c94
Jerry Kiely <jerry@cowboysmall.com>
2021-12-31 17:09:31 +0000

    some cleanup + added some docs

--- challenges/disorderly-escape/solution.py
+++ challenges/disorderly-escape/solution.py
@@ -7,10 +7,33 @@ def main(argv):
 
     print("")
     print("Solution 1: ")
-    print("     input: 2, 3, 4")
-    print("  expected: 430")
-    print("    actual: {}".format(solution_01.solution(2, 3, 4)))
+    print("     input: 1, 3, 2")
+    print("  expected: 4")
+    print("    actual: {}".format(solution_01.solution(1, 3, 2)))
     print("")
+
+    print("")
+    print("Solution 1: ")
+    print("     input: 1, 4, 2")
+    print("  expected: 5")
+    print("    actual: {}".format(solution_01.solution(1, 4, 2)))
+    print("")
+
+    print("")
+    print("Solution 1: ")
+    print("     input: 1, 5, 2")
+    print("  expected: 6")
+    print("    actual: {}".format(solution_01.solution(1, 5, 2)))
+    print("")
+
+    print("")
+    print("Solution 1: ")
+    print("     input: 1, 6, 2")
+    print("  expected: 7")
+    print("    actual: {}".format(solution_01.solution(1, 6, 2)))
+    print("")
+
+
 
     print("")
     print("Solution 1: ")
@@ -21,9 +44,138 @@ def main(argv):
 
     print("")
     print("Solution 1: ")
+    print("     input: 2, 2, 3")
+    print("  expected: 27")
+    print("    actual: {}".format(solution_01.solution(2, 2, 3)))
+    print("")
+
+    print("")
+    print("Solution 1: ")
+    print("     input: 2, 2, 4")
+    print("  expected: 76")
+    print("    actual: {}".format(solution_01.solution(2, 2, 4)))
+    print("")
+
+
+
+    print("")
+    print("Solution 1: ")
+    print("     input: 2, 3, 2")
+    print("  expected: 13")
+    print("    actual: {}".format(solution_01.solution(2, 3, 2)))
+    print("")
+
+    print("")
+    print("Solution 1: ")
+    print("     input: 2, 3, 3")
+    print("  expected: 92")
+    print("    actual: {}".format(solution_01.solution(2, 3, 3)))
+    print("")
+
+    print("")
+    print("Solution 1: ")
+    print("     input: 2, 3, 4")
+    print("  expected: 430")
+    print("    actual: {}".format(solution_01.solution(2, 3, 4)))
+    print("")
+
+    print("")
+    print("Solution 1: ")
+    print("     input: 2, 3, 5")
+    print("  expected: 1505")
+    print("    actual: {}".format(solution_01.solution(2, 3, 5)))
+    print("")
+
+    print("")
+    print("Solution 1: ")
+    print("     input: 2, 3, 6")
+    print("  expected: 4291")
+    print("    actual: {}".format(solution_01.solution(2, 3, 6)))
+    print("")
+
+    print("")
+    print("Solution 1: ")
+    print("     input: 2, 3, 7")
+    print("  expected: 10528")
+    print("    actual: {}".format(solution_01.solution(2, 3, 7)))
+    print("")
+
+
+
+    print("")
+    print("Solution 1: ")
     print("     input: 2, 4, 2")
     print("  expected: 22")
     print("    actual: {}".format(solution_01.solution(2, 4, 2)))
+    print("")
+
+
+
+    print("")
+    print("Solution 1: ")
+    print("     input: 3, 3, 2")
+    print("  expected: 36")
+    print("    actual: {}".format(solution_01.solution(3, 3, 2)))
+    print("")
+
+    print("")
+    print("Solution 1: ")
+    print("     input: 3, 3, 3")
+    print("  expected: 738")
+    print("    actual: {}".format(solution_01.solution(3, 3, 3)))
+    print("")
+
+    print("")
+    print("Solution 1: ")
+    print("     input: 3, 3, 4")
+    print("  expected: 8240")
+    print("    actual: {}".format(solution_01.solution(3, 3, 4)))
+    print("")
+
+
+
+    print("")
+    print("Solution 1: ")
+    print("     input: 4, 4, 2")
+    print("  expected: 317")
+    print("    actual: {}".format(solution_01.solution(4, 4, 2)))
+    print("")
+
+    print("")
+    print("Solution 1: ")
+    print("     input: 4, 4, 3")
+    print("  expected: 90492")
+    print("    actual: {}".format(solution_01.solution(4, 4, 3)))
+    print("")
+
+    print("")
+    print("Solution 1: ")
+    print("     input: 4, 4, 4")
+    print("  expected: 7880456")
+    print("    actual: {}".format(solution_01.solution(4, 4, 4)))
+    print("")
+
+
+
+    print("")
+    print("Solution 1: ")
+    print("     input: 5, 5, 2")
+    print("  expected: 5624")
+    print("    actual: {}".format(solution_01.solution(5, 5, 2)))
+    print("")
+
+    print("")
+    print("Solution 1: ")
+    print("     input: 5, 5, 3")
+    print("  expected: 64796982")
+    print("    actual: {}".format(solution_01.solution(5, 5, 3)))
+    print("")
+
+    print("")
+    print("Solution 1: ")
+    print("     input: 5, 5, 4")
+    print("  expected: 79846389608")
+    print("    actual: {}".format(solution_01.solution(5, 5, 4)))
     print("")
 
 
--- challenges/disorderly-escape/solution_01.py
+++ challenges/disorderly-escape/solution_01.py
@@ -14,42 +14,47 @@ def solution(w, h, s):
     def lcm(p, q):
         return p * q // gcd(p, q)
 
-    def cycle_combs(n):
+    def cycles(n):
         c = []
-        cycle_combs_rec(1, n, 0, c)
+        t = []
+
+        def cycles_rec(i, n, l):
+            if n == 0:
+                c.append(list(Counter(t[:l]).items()))
+
+            for j in range(i, n + 1):
+                t.append(j)
+                cycles_rec(j, n - j, l + 1)
+                t.pop()
+
+        cycles_rec(1, n, 0)
+
         return c
 
-    def cycle_combs_rec(i, n, l, c, t = []):
-        if n == 0:
-            c.append(list(Counter(t[:l]).items()))
-
-        for j in range(i, n + 1):
-            t.append(j)
-            cycle_combs_rec(j, n - j, l + 1, c, t)
-            t.pop()
-
-    def calc_coeff(c):
-        cf = 1
+    def denominator(c):
+        d = 1
         for v, f in c:
-            cf *= v**f
-            cf *= factorial(f)
-        return cf
+            d *= v**f * factorial(f)
+        return d
+
+    def numerator(c1, c2, s):
+        n = 1
+        for v1, f1 in c1:
+            for v2, f2 in c2:
+                n *= s**((v1 * f1 * v2 * f2) // lcm(v1, v2))
+        return n
 
 
-    row_cycles = cycle_combs(h)
-    col_cycles = cycle_combs(w)
-    row_coeffs = [calc_coeff(c) for c in row_cycles]
-    col_coeffs = [calc_coeff(c) for c in col_cycles]
+    row_cycles = cycles(h)
+    col_cycles = cycles(w)
+
+    row_denoms = [denominator(rc) for rc in row_cycles]
+    col_denoms = [denominator(cc) for cc in col_cycles]
 
     total = 0
 
-    for row_cycle, row_coeff in zip(row_cycles, row_coeffs):
-        for col_cycle, col_coeff in zip(col_cycles, col_coeffs):
-            term = Fraction(1, col_coeff * row_coeff)
-            for v1, f1 in row_cycle:
-                for v2, f2 in col_cycle:
-                    exp   = (v1 * f1 * v2 * f2) // lcm(v1, v2)
-                    term *= s**exp
-            total += term
+    for row_cycle, row_denom in zip(row_cycles, row_denoms):
+        for col_cycle, col_denom in zip(col_cycles, col_denoms):
+            total += Fraction(numerator(row_cycle, col_cycle, s), row_denom * col_denom)
 
     return str(total)
