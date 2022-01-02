3bd3fcf7e4c4df6757c31e58cf8b78ba54b69afe
Jerry Kiely <jerry@cowboysmall.com>
2022-01-02 11:28:27 +0000

    some cleanup

--- challenges/disorderly-escape/solution_01.py
+++ challenges/disorderly-escape/solution_01.py
@@ -18,15 +18,14 @@ def solution(w, h, s):
 
     def cycles(n):
         c = []
-        t = []
 
-        def cycles_rec(i, n, l):
+        def cycles_rec(i, n, l, t = []):
             if n == 0:
                 c.append(list(Counter(t[:l]).items()))
 
             for j in range(i, n + 1):
                 t.append(j)
-                cycles_rec(j, n - j, l + 1)
+                cycles_rec(j, n - j, l + 1, t)
                 t.pop()
 
         cycles_rec(1, n, 0)
